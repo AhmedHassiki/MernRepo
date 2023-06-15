e8744b4974b708ddc4c5877700d0f91627b3e520
Ahmed Hassiki <hassiki.services@gmail.com>
2023-06-15 19:33:26 +0100

    addingFrontURLtobackend

--- server/server.js
+++ server/server.js
@@ -3,13 +3,19 @@ const Products = require('./routes/product')
 const User = require('./routes/userRoute')
 const Cart = require('./routes/cartRoute')
 const Order = require("./routes/orderRoute");
+const cors = require("cors");
+
 
 const app = express();
 require('dotenv').config();
 const connectDB = require('./config/connectDB');
 connectDB();
-
+// middleware
+const corsOptions = {
+    origin: "https://authentikey.onrender.com/", // frontend URI (ReactJS)
+}
 app.use(express.json());
+app.use(cors(corsOptions));
 app.use('/api/product', Products);
 app.use('/api/auth', User );
 app.use('/api/cart', Cart );
